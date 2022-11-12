807d760fd4053382a78e4edb6159aa60d68ac64e
Adib El Ounani <wananiadib@gmail.com>
2022-11-12 14:54:37 -0500

    fixed folder place

--- app.mjs
+++ app.mjs
@@ -18,7 +18,7 @@ const sessionOptions = {
 };
 // Production 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
 }
 app.use(session(sessionOptions));
 app.use(express.json());
